bfcca3135e0e4c5ef6559d71afdd4639c9c7d2f2
cbobio <cbobio3@gmail.com>
2022-08-11 19:08:15 -0300

    verificar3

--- src/main/java/one/digitalinivation/parking/CloudParkingApplication.java
+++ src/main/java/one/digitalinivation/parking/CloudParkingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudParkingApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CloudParkingApplication.class, args);
 	}
